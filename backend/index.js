fc86833ae633678b25d99ee37bd847bdcd53b3fd
anasayubi <anasayubi@hotmail.com>
2017-05-21 11:23:17 +0500

    add editMovie API request to backend

--- backend/index.js
+++ backend/index.js
@@ -30,11 +30,15 @@ app.post('/api/addMovie', function(req, res) {
   else if(typeof(req.body.title) !== 'string'){
     res.status(400).json({"code": 400, "msg": "'title' field must be of type String"});
   }
-  // ensure 'releaseYear' is of type String convertible to Date 
+  // ensure 'releaseYear' is of type String
+  else if(req.body.releaseYear && typeof(req.body.releaseYear) !== 'string'){
+    res.status(400).json({"code": 400, "msg": "'releaseYear' field must be of type String"});
+  }
+  // ensure 'releaseYear' is convertible to Date 
   //   IF 'releaseYear' is present
   //   ensure by converting string representation of date and checking if it is invalid
   else if(req.body.releaseYear && (new Date(req.body.releaseYear)).toString() === 'Invalid Date'){
-    res.status(400).json({"code": 400, "msg": "'releaseYear' field must be of type String convertible to date"});
+    res.status(400).json({"code": 400, "msg": "'releaseYear' field must be convertible to date"});
   }
   // ensure 'rating' is an Integer
   //   IF 'rating' is present
@@ -78,6 +82,10 @@ app.post('/api/removeMovie', function(req, res) {
   if(!req.body.id){
     res.status(400).json({"code": 400, "msg": "'id' field must be present"});
   }
+  // ensure that 'id' is of type String
+  else if(typeof(req.body.id) !== 'string'){
+    res.status(400).json({"code": 400, "msg": "'id' field must be of type String"});
+  }
   else{
     Movie.findByIdAndRemove(req.body.id, function(err, writeResult){
       console.log('err: ', err);
@@ -93,6 +101,97 @@ app.post('/api/removeMovie', function(req, res) {
       }
     })
   }
+});
+
+app.post('/api/editMovie', function(req, res){
+  // Accepts JSON input as such:
+  //   {id: [ObjectID], title: [String], releaseYear: [String], rating: [Number]}
+  // 'id' is required as it identifies which movie must be edited
+  // both 'releaseYear' & 'rating' are not required
+  // 'releaseYear' is a string that must be converted to date
+  //   stored as an object in MongoDB
+  // 'rating' is a number that spans from 1 to 10 and must be an integer
+
+  // Use 'movieUpdateInfo' to store all update info
+  var movieUpdateInfo = {};
+
+  if(req.body.id){
+    // ensure that 'id' is of type String
+    if(typeof(req.body.id) !== 'string'){
+      res.status(400).json({"code": 400, "msg": "'id' field must be of type string"});
+      return;
+    }
+    // store 'id' in case all above validation checks are passed
+    movieUpdateInfo.id = req.body.id;
+  }
+  else{
+    res.status(400).json({"code": 400, "msg": "'id' field must be present"});
+    return;
+  }
+
+  // ensure at least one of 'title', 'releaseYear' or 'rating' is present
+  if(!req.body.releaseYear && !req.body.title && !req.body.rating){
+    res.status(400).json({"code": 400, "msg": "at least one of 'title', 'releaseYear' or 'rating' must be present"});
+    return;
+  }
+
+  // validation when 'title' is present
+  if(req.body.title){
+    // ensure 'title' is of type String
+    if(typeof(req.body.title) !== 'string'){
+      res.status(400).json({"code": 400, "msg": "'title' field must be of type String"});
+      return;
+    }
+    // store 'title' in case all above validation checks are passed
+    movieUpdateInfo.title = req.body.title;
+  }
+
+  // validation when 'releaseYear' is present
+  if(req.body.releaseYear){
+    // ensure 'releaseYear' is of type String
+    if(typeof(req.body.releaseYear) !== 'string'){
+      res.status(400).json({"code": 400, "msg": "'releaseYear' field must be of type String"});
+      return;
+    }
+    // ensure 'releaseYear' is convertible to Date 
+    //   ensure by converting string representation of date and checking if it is invalid or valid
+    else if((new Date(req.body.releaseYear)).toString() === 'Invalid Date'){
+      res.status(400).json({"code": 400, "msg": "'releaseYear' field must be convertible to date"});
+      return;
+    }
+    // store 'releaseYear' in case all above validation checks are passed
+    movieUpdateInfo.releaseYear = req.body.releaseYear;
+  }
+
+  // validation when 'rating' is present
+  if(req.body.rating){
+    // ensure 'rating' is an Integer
+    if(!Number.isInteger(req.body.rating)){
+      res.status(400).json({"code": 400, "msg": "'rating' field must be an Integer"});
+      return;
+    }
+    // ensure 1 <= 'rating' <= 10 
+    else if(req.body.rating < 1 || req.body.rating > 10){
+      res.status(400).json({"code": 400, "msg": "'rating' field must lie in [1, 10]"});
+      return;
+    }
+    // store 'rating' in case all above validation checks are passed
+    movieUpdateInfo.rating = req.body.rating;
+  }
+  
+  // Process saving if all validation checks passed
+  console.log('movieUpdateInfo: ', movieUpdateInfo);
+
+  Movie.findByIdAndUpdate(req.body.id, movieUpdateInfo, function(err, doc){
+    console.log('err: ', err);
+    console.log('doc: ', doc);
+    if(!err && doc){
+      res.status(200).json({"code": 200, "msg": "movie edited"});
+    }
+    else{
+      res.status(400).json({"code": 400, "msg": "movie unable to be edited"});
+    }
+  });
 });
   
 app.listen(9000, function(){
