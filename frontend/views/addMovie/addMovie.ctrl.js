c475d723d05d4fdd0dd6a5a216c3caedef69ef46
anasayubi <anasayubi@hotmail.com>
2017-06-12 15:24:46 +0500

    connec add movie front and backend along with validation

--- frontend/views/addMovie/addMovie.ctrl.js
+++ frontend/views/addMovie/addMovie.ctrl.js
@@ -1,34 +1,153 @@
 angular.module('app')
-.controller('addMovieCtrl', ['$http', 'BACKEND_SERVER_DOMAIN', 'BACKEND_SERVER_PORT', 
+.controller('addMovieCtrl', ['$scope', '$http', 'BACKEND_SERVER_DOMAIN', 'BACKEND_SERVER_PORT', 
 'BACKEND_SERVER_PROTOCOL',
-function($http, BSD, BSPORT, BSPROT){
+function($scope, $http, BSD, BSPORT, BSPROT){
   var self = this;
   // indicates whether the form has been submitted or not
   self.submitted = false;
+  // reveals success message
+  self.success = false;
+  // reveals server error
+  self.serverError = false;
+
+  // Checkers:
+  //   Remove validation messages
+  self.checkTitle = function(){
+    // Only remove validation checks when at least submitted once
+    if(self.submitted){
+      if(!self.addMovieForm.title.$error.required){
+        self.titleRequiredError = false;
+      }
+    }
+  }
+
+  self.checkReleaseYear = function(){
+    // Only remove validation checks when at least submitted once
+    if(self.submitted){
+      if(!self.addMovieForm.releaseYear.$error.number){
+        self.releaseYearNumberError = false;
+      }
+      if(!self.addMovieForm.releaseYear.$error.min){
+        self.releaseYearMinError = false;
+      }
+      if(!self.addMovieForm.releaseYear.$error.max){
+        self.releaseYearMaxError = false;
+      }
+      if(!self.addMovieForm.releaseYear.$error.step){
+        self.releaseYearStepError = false;
+      }
+      if(!self.addMovieForm.releaseYear.$error.number && !self.addMovieForm.releaseYear.$error.min 
+      && !self.addMovieForm.releaseYear.$error.max && !self.addMovieForm.releaseYear.$error.step){
+        self.releaseYearError = false;
+      }
+    }
+  }
+
+  self.checkRating =  function(){
+    // Only remove validation checks when at least submitted once
+    if(self.submitted){
+      if(!self.addMovieForm.rating.$error.number){
+        self.ratingNumberError = false;
+      }
+      if(!self.addMovieForm.rating.$error.min){
+        self.ratingMinError = false;
+      }
+      if(!self.addMovieForm.rating.$error.max){
+        self.ratingMaxError = false;
+      }
+      if(!self.addMovieForm.rating.$error.step){
+        self.ratingStepError = false;
+      }
+      if(!self.addMovieForm.rating.$error.number && !self.addMovieForm.rating.$error.min 
+      && !self.addMovieForm.rating.$error.max && !self.addMovieForm.rating.$error.step){
+        self.ratingError = false;
+      }
+    }
+  }
+
   self.submit = function(){
     // form has been submitted hence true 
     self.submitted = true;
-    // if port is a non-empty string then set requestURL as such
-    if(BSPORT){
-      var requestURL = BSPROT + '://' + BSD + ':' + BSPORT + '/api/movie';
-      // console.log(requestURL)
+
+    // refresh all errors before setting them
+    self.titleRequiredError = false;
+    self.releaseYearNumberError = false;
+    self.releaseYearMinError = false;
+    self.releaseYearMaxError = false;
+    self.releaseYearStepError = false;
+    self.releaseYearStepError = false;
+    self.ratingMinError = false;
+    self.ratingMaxError = false;
+    self.ratingStepError = false;
+
+    // ERRORS
+    // title:
+    if(self.addMovieForm.title.$error.required){
+      self.titleRequiredError = true;
     }
-    // if port is an empty string then set requestURL as such
-    else{
-      var requestURL = BSPROT + '://' + BSD + '/api/movie';
-      // console.log(requestURL)
+    // releaseYear:
+    if(self.addMovieForm.releaseYear.$error.number){
+      self.releaseYearNumberError = true;
+      self.releaseYearError = true;
     }
-    var data = {
-      title: self.title,
-      releaseYear: self.releaseYear,
-      rating: self.rating 
-    };
-    
-    $http.post(requestURL, data).then(function success(resp){
-      console.log('resp: ', resp);
-    }, function err(resp){
-      console.log('err: ', resp);
-    })
+    if(self.addMovieForm.releaseYear.$error.min){
+      self.releaseYearMinError = true;
+      self.releaseYearError = true;
+    }
+    if(self.addMovieForm.releaseYear.$error.max){
+      self.releaseYearMaxError = true;
+      self.releaseYearError = true;
+    }
+    if(self.addMovieForm.releaseYear.$error.step){
+      self.releaseYearStepError = true;
+      self.releaseYearError = true;
+    }
+    // rating:
+    if(self.addMovieForm.rating.$error.number){
+      self.ratingNumberError = true;
+      self.ratingError = true;
+    }
+    if(self.addMovieForm.rating.$error.min){
+      self.ratingMinError = true;
+      self.ratingError = true;
+    }
+    if(self.addMovieForm.rating.$error.max){
+      self.ratingMaxError = true;
+      self.ratingError = true;
+    }
+    if(self.addMovieForm.rating.$error.step){
+      self.ratingStepError = true;
+      self.ratingError = true;
+    }
+
+    // If no errors are present
+    if(!self.ratingError && !self.releaseYearError && !self.titleRequiredError){
+      // if port is a non-empty string then set requestURL as such
+      if(BSPORT){
+        var requestURL = BSPROT + '://' + BSD + ':' + BSPORT + '/api/movie';
+        // console.log(requestURL)
+      }
+      // if port is an empty string then set requestURL as such
+      else{
+        var requestURL = BSPROT + '://' + BSD + '/api/movie';
+        // console.log(requestURL)
+      }
+
+      var data = {
+        title: self.title,
+        releaseYear: String(self.releaseYear),
+        rating: self.rating 
+      };
+
+      $http.post(requestURL, data).then(function success(resp){
+        console.log('resp: ', resp);
+        // show success message
+        self.success = true;
+      }, function err(resp){
+        console.log('err: ', resp);
+        self.serverError = true;
+      })
+    }
   }
 }])
 
